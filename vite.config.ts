60e8ff5e2256963c0f05b6ab0228941489a741d1
André Mathlener <andre.mathlener@ns.nl>
2024-10-17 15:31:41 +0200

    Ignore coverage in src/domain/models.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		include: ['test/**/*.test.ts']
+		include: ['test/**/*.test.ts'],
+		coverage: {
+			enabled: true,
+			all: true,
+			provider: 'v8',
+			reporter: ['text', 'lcov', 'html'],
+			exclude: [...configDefaults.exclude, 'src/domain/models.ts'],
+		},
 	}
 });
