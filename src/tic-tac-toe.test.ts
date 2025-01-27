112cec4865a952df6899adca78aa596d29ff485c
André Mathlener <andre.mathlener@ns.nl>
2025-01-27 15:55:58 +0100

    Refactoring: only instantiate TicTacToe once

--- src/tic-tac-toe.test.ts
+++ src/tic-tac-toe.test.ts
@@ -2,20 +2,19 @@ import { describe, expect, it } from 'vitest';
 import { TicTacToe } from './tic-tac-toe';
 
 describe('Tic Tac Toe', () => {
+    const ticTacToe = new TicTacToe();
+
     it('Should create an instance of the TicTacToe class', () => {
-        const ticTacToe = new TicTacToe();
         expect(ticTacToe).toBeDefined();
     });
 
     it('Should make first player X', () => {
-        const ticTacToe = new TicTacToe();
-
         const player = ticTacToe.getCurrentPlayer();
+
         expect(player).toBe('X');
     });
 
     it('Should make second player O', () => {
-        const ticTacToe = new TicTacToe();
         ticTacToe.makeMove();
 
         const player = ticTacToe.getCurrentPlayer();
