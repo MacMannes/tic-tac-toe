739cddf228c1e7a25a4a7024b019bb5d37b0c473
André Mathlener <andre.mathlener@ns.nl>
2025-01-27 15:35:54 +0100

    Change description of test

--- src/tic-tac-toe.test.ts
+++ src/tic-tac-toe.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { TicTacToe } from './tic-tac-toe';
 
 describe('Tic Tac Toe', () => {
-    it('Creating an instance of the TicTacToe class should work', () => {
+    it('Should create an instance of the TicTacToe class', () => {
         const ticTacToe = new TicTacToe();
         expect(ticTacToe).toBeDefined();
     });
